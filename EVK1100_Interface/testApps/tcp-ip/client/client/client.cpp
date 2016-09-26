bfd15ab2bf032393bda6b77050cbf21cb8b17d32
peter <peter.niederer@sonnenkinder.org>
2016-09-27 01:54:29 +0200

    c++ application compiled on VC++ 15.0

--- EVK1100_Interface/testApps/tcp-ip/client/client/client.cpp
+++ EVK1100_Interface/testApps/tcp-ip/client/client/client.cpp
@@ -15,16 +15,137 @@
 #pragma comment(lib,"ws2_32.lib")
 
 #define BufferSIZE 500
+#define clientAddress    "168.192.0.151"
+#define clientPort   "1234"
 
 
 void usage(void);
+SOCKET rustlightSocket;	
+sockaddr_in server;
+sockaddr_in client;
 
-						
-			
-	SOCKET rustlightSocket;					
-	sockaddr_in server;
-	sockaddr_in client;
 
+	void  err_printf(char *emsg, ...)
+	{
+		va_list ap;
+
+		va_start(ap, emsg);
+		fprintf(stderr, emsg, ap);
+		va_end(ap);
+	}
+
+	void exitRustlightTcpIp()
+	{
+		closesocket(rustlightSocket);
+		err_printf( "socket closed\n");
+		WSACleanup();
+		err_printf( "aft wsa cleanup\n");
+	}
+
+	int initRustlightTcpIp(char* cliAddress, char* cliPort)
+	{
+		int res = 0;
+		int a1, a2, a3, a4;
+		hostent *hostP;
+		char host_name[256];
+		unsigned int port_number;
+		WSADATA rustlightWsDATA;
+		DWORD  sotimeout = 0x00000400;
+		
+
+
+		if (sscanf_s((const char *)cliAddress, "%d.%d.%d.%d", &a1, &a2, &a3, &a4) != 4)
+		{
+			err_printf( "address error.\n");
+			++ res;
+		}
+		if (sscanf_s((const char *)cliPort, "%u", &port_number) != 1)
+		{
+			err_printf( "portnumber error.\n");
+			++ res;
+		}
+
+		err_printf( "bef wsa startup\n");
+		if (WSAStartup(0x0101, &rustlightWsDATA) != 0)
+		{
+			err_printf( "Could not open Windows connection.\n");
+			++ res;
+		}
+		err_printf( "WSA Started up\n");
+
+
+		rustlightSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+		if (rustlightSocket == INVALID_SOCKET)
+		{
+			err_printf( "Could not create socket.\n");
+			WSACleanup();
+			++ res;
+		}
+		err_printf( "created  socket\n");
+
+		memset((void *)&server, '\0', sizeof(struct sockaddr_in));
+
+		server.sin_family = AF_INET;
+		server.sin_port = htons(port_number);
+
+		server.sin_addr.S_un.S_un_b.s_b1 = (unsigned char)a1;
+		server.sin_addr.S_un.S_un_b.s_b2 = (unsigned char)a2;
+		server.sin_addr.S_un.S_un_b.s_b3 = (unsigned char)a3;
+		server.sin_addr.S_un.S_un_b.s_b4 = (unsigned char)a4;
+
+		memset((void *)&client, '\0', sizeof(struct sockaddr_in));
+
+		client.sin_family = AF_INET;
+		client.sin_port = htons(0);
+
+		gethostname(host_name, sizeof(host_name));
+		hostP = gethostbyname(host_name);
+
+		if (hostP == NULL)
+		{
+			err_printf( "Could not get host name.\n");
+			closesocket(rustlightSocket);
+			WSACleanup();
+			++ res;
+		}
+
+		err_printf( "host with name %s got addr %u.%u.%u.%u\n", host_name,
+			(unsigned char)hostP->h_addr_list[0][0], (unsigned char)hostP->h_addr_list[0][1],
+			(unsigned char)hostP->h_addr_list[0][2], (unsigned char)hostP->h_addr_list[0][3]);
+
+		client.sin_addr.S_un.S_un_b.s_b1 = (unsigned char)hostP->h_addr_list[0][0];
+		client.sin_addr.S_un.S_un_b.s_b2 = (unsigned char)hostP->h_addr_list[0][1];
+		client.sin_addr.S_un.S_un_b.s_b3 = (unsigned char)hostP->h_addr_list[0][2];
+		client.sin_addr.S_un.S_un_b.s_b4 = (unsigned char)hostP->h_addr_list[0][3];
+
+		if (bind(rustlightSocket, (struct sockaddr *)&client, sizeof(struct sockaddr_in)) == -1)
+		{
+			err_printf( "Cannot bind address to socket.\n");
+			closesocket(rustlightSocket);
+			WSACleanup();
+			++ res;
+		}
+
+		err_printf( "aft socket bind to ip\n");
+
+		res = setsockopt(rustlightSocket, IPPROTO_UDP, SO_SNDTIMEO, (char*)&sotimeout, sizeof(sotimeout));
+		if (res != 0) {
+			err_printf( "Error setting send timeout data.\n");
+			closesocket(rustlightSocket);
+			WSACleanup();
+			++ res;
+		}
+
+		res = setsockopt(rustlightSocket, IPPROTO_UDP, SO_RCVTIMEO, (char*)&sotimeout, sizeof(sotimeout));
+		if (res != 0) {
+			err_printf( "Error setting receive timeout data.\n");
+			closesocket(rustlightSocket);
+			WSACleanup();
+			++ res;
+		}
+
+		return res;
+	}
 									
 
 
@@ -45,167 +166,46 @@ void usage(void);
 			cnt = cnt + 1;
 		}*/
 		
+		snprintf(send_buffer, sizeof(send_buffer), "%X\n", msg);
 
-		fprintf(stderr, "sending message: %s \n", send_buffer);
+		err_printf( "sending message: %s \n", send_buffer);
 
 
 		server_length = sizeof(struct sockaddr_in);
 		if ((res=sendto(rustlightSocket, send_buffer, (int)strlen(send_buffer) + 1, 0, (struct sockaddr *)&server, server_length)) == -1)
 		{
-			fprintf(stderr, "Error transmitting data.\n");
+			err_printf( "Error transmitting data.\n");
 			closesocket(rustlightSocket);
 			WSACleanup();
 			return res;
 		}
-		fprintf(stderr, "message sent, now receiving (without timeout).... \n");
+		err_printf( "message sent, now receiving (without timeout).... \n");
 
 		memset(receive_buffer, 0, sizeof(receive_buffer));
 
 		if ((res = recvfrom(rustlightSocket, (char *)&receive_buffer, (int)sizeof(receive_buffer), 0, (struct sockaddr *)&server, &server_length)) < 0)
 		{
-			fprintf(stderr, "Error receiving data.\n");
+			err_printf( "Error receiving data.\n");
 			closesocket(rustlightSocket);
 			WSACleanup();
-			exit(0);
+			return res;
 		}
-		fprintf(stderr, "socket received\n");
-		fprintf(stderr, "received: %s\n", receive_buffer);
-
+		err_printf( "socket received\n");
+		err_printf( "received: %s\n", receive_buffer);
+		return res;
 	}
 
 int main(int argc, char **argv)
 {
-	int cnt;
-	int a1, a2, a3, a4;
-//	int b1, b2, b3, b4;
-	hostent *hp;
-	char host_name[256];
-	unsigned short port_number;
-	WSADATA rustlightWsDATA;
-	DWORD  sotimeout = 0x00000400;
-
-	for (cnt = 0;cnt < argc; ++ cnt) {
-		fprintf(stderr,"arg %i %s len %i\n",cnt,(const char *)argv[cnt],strlen((const char *)argv[cnt]));
-	}
+	int res = 0;
 	
+	res = initRustlightTcpIp(clientAddress, clientPort);
 
-	if (argc < 3 )
-	{
-		usage();
-	}
-	if (sscanf_s((const char *)argv[1], "%d.%d.%d.%d", &a1, &a2, &a3, &a4) != 4)
-	{
-		usage();
-	}
-	fprintf(stderr,"arg 2 %s \n",argv[2]);
-	if (sscanf_s((const char *)argv[2], "%u", &port_number) != 1)
-	{
-		usage();
+	if (res == 0) {
+		res = communicateMsg(0x010202a5);
 	}
 
-	fprintf(stderr,"bef wsa startup\n");
-	if (WSAStartup(0x0101, &rustlightWsDATA) != 0)
-	{
-		fprintf(stderr, "Could not open Windows connection.\n");
-		exit(0);
-	}
-	fprintf(stderr,"WSA Started up\n");
-
-
-	rustlightSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (rustlightSocket == INVALID_SOCKET)
-	{
-		fprintf(stderr, "Could not create socket.\n");
-		WSACleanup();
-		exit(0);
-	}
-	fprintf(stderr,"created  socket\n");
-
-
-
-	memset((void *)&server, '\0', sizeof(struct sockaddr_in));
-
-	
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port_number);
-
-	server.sin_addr.S_un.S_un_b.s_b1 = (unsigned char)a1;
-	server.sin_addr.S_un.S_un_b.s_b2 = (unsigned char)a2;
-	server.sin_addr.S_un.S_un_b.s_b3 = (unsigned char)a3;
-	server.sin_addr.S_un.S_un_b.s_b4 = (unsigned char)a4;
-
-	
-	memset((void *)&client, '\0', sizeof(struct sockaddr_in));
-
-	
-	client.sin_family = AF_INET;
-	client.sin_port = htons(0);
-
-
-		gethostname(host_name, sizeof(host_name));
-		hp = gethostbyname(host_name);
-
-	
-		if (hp == NULL)
-		{
-			fprintf(stderr, "Could not get host name.\n");
-			closesocket(rustlightSocket);
-			WSACleanup();
-			exit(0);
-		}
-
-		fprintf(stderr,"host with name %s got addr %u.%u.%u.%u\n",host_name,
-						(unsigned char)hp->h_addr_list[0][0],(unsigned char)hp->h_addr_list[0][1],
-						(unsigned char)hp->h_addr_list[0][2],(unsigned char)hp->h_addr_list[0][3]);
-
-
-		
-
-		client.sin_addr.S_un.S_un_b.s_b1 = (unsigned char)hp->h_addr_list[0][0];
-		client.sin_addr.S_un.S_un_b.s_b2 = (unsigned char)hp->h_addr_list[0][1];
-		client.sin_addr.S_un.S_un_b.s_b3 = (unsigned char)hp->h_addr_list[0][2];
-		client.sin_addr.S_un.S_un_b.s_b4 = (unsigned char)hp->h_addr_list[0][3];
-
-		if (bind(rustlightSocket, (struct sockaddr *)&client, sizeof(struct sockaddr_in)) == -1)
-		{
-			fprintf(stderr, "Cannot bind address to socket.\n");
-			closesocket(rustlightSocket);
-			WSACleanup();
-			exit(0);
-		}
-
-		fprintf(stderr, "aft socket bind to ip\n");
-
-		int res = setsockopt(rustlightSocket, IPPROTO_UDP, SO_SNDTIMEO, (char*)&sotimeout, sizeof(sotimeout));
-		if (res != 0) {
-			fprintf(stderr, "Error setting send timeout data.\n");
-			closesocket(rustlightSocket);
-			WSACleanup();
-			exit(0);
-		}
-
-		res = setsockopt(rustlightSocket, IPPROTO_UDP, SO_RCVTIMEO, (char*)&sotimeout, sizeof(sotimeout));
-		if (res != 0) {
-			fprintf(stderr, "Error setting receive timeout data.\n");
-			closesocket(rustlightSocket);
-			WSACleanup();
-			exit(0);
-		}
-
-		res = communicateMsg(0x0102a5a5);
-
-
-
-	closesocket(rustlightSocket);
-	fprintf(stderr,"socket closed\n");
-	WSACleanup();
-	fprintf(stderr,"aft wsa cleanup\n");
+	exitRustlightTcpIp();
+	err_printf("aft wsa cleanup\n");
 	return(0);
 }
-
-void usage(void)
-{
-	fprintf(stderr, "Usage: client server_address port message to server\n");
-	exit(0);
-}
-
