afc4a443d4a0bbc0b215580b58aa3df30943aafd
peter <peter.niederer@sonnenkinder.org>
2020-01-08 03:14:48 +0100

    implementing hygrosens uart receive

--- EVK1100_Interface/asf/UartPrint/UartHygrosens.c
+++ EVK1100_Interface/asf/UartPrint/UartHygrosens.c
@@ -147,26 +147,6 @@ CPU_INT08U init_HygrosenseReceiver()
 	CPU_INT08U  err_init_hygrosens = OS_NO_ERR;
 	
 	if (err_init_hygrosens == OS_NO_ERR) {
-		BSP_USART_Init (HYGROSENS_USART_COM, hygrosensUsartSpeed);  
-
-//		if (!(SerialPrintQSem = OSSemCreate(0)))
-		// after initialisation, probable a character can be sent
-		// worst case we just loose it
-//		{
-			//err_printf("could not create serialQSem\n");
-			err_init_hygrosens = 0xFF;
-//		}
-	}
-	if (err_init_hygrosens == OS_NO_ERR) {
-		if (BSP_INTC_IntReg(&hygrosensRxTxISR, HYGROSENS_USART_IRQ, 1) == BSP_INTC_ERR_NONE ) {
-			err_init_hygrosens = OS_NO_ERR;
-			} else {
-			err_init_hygrosens = 0xFF;
-		}
-		BSP_USART_IntEn (HYGROSENS_USART_COM, (1<< AVR32_USART_IER_RXRDY));
-	}
-	
-	if (err_init_hygrosens == OS_NO_ERR) {
 		hygrosensMsgMem = OSMemCreate(&hygrosensMem[0], hygrosensMemorySz, sizeof(hygrosensStringMemory), &err_init_hygrosens);
 	}
 	if (err_init_hygrosens == OS_NO_ERR) {
@@ -176,7 +156,6 @@ CPU_INT08U init_HygrosenseReceiver()
 		hygrosensTaskMsgQ = OSQCreate(&hygrosensTaskMsgs[0], hygrosensMemorySz);
 		if (! hygrosensTaskMsgQ) err_init_hygrosens = 0xFF;
 	}
-	
 	
 	if (err_init_hygrosens == OS_NO_ERR )  {
 		err_init_hygrosens = OSTaskCreateExt(SerialHygrosensThreadMethod,
@@ -189,75 +168,23 @@ CPU_INT08U init_HygrosenseReceiver()
 		(void *)0,
 		OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
 	}
-	
-	if (err_init_hygrosens == OS_NO_ERR )  {	
+		
+	if (err_init_hygrosens == OS_NO_ERR )  {
 		OSTaskNameSet(SERIAL_HYGROSENS_TASK_PRIO, (INT8U *)"HygrosensTask", &err_init_hygrosens);
+	}
+	
+	if (err_init_hygrosens == OS_NO_ERR) {
+		BSP_USART_Init (HYGROSENS_USART_COM, hygrosensUsartSpeed);  
+	}
+	if (err_init_hygrosens == OS_NO_ERR) {
+		if (BSP_INTC_IntReg(&hygrosensRxTxISR, HYGROSENS_USART_IRQ, 1) == BSP_INTC_ERR_NONE ) {
+			err_init_hygrosens = OS_NO_ERR;
+			} else {
+			err_init_hygrosens = 0xFF;
+		}
+		BSP_USART_IntEn (HYGROSENS_USART_COM, (1<< AVR32_USART_IER_RXRDY));
 	}
 	serialHygrosensOn = (err_init_hygrosens == OS_NO_ERR);
 	
-	
-/*	serialPrintOn = 0;
-	amtErrorPrintfCalls = 0;
-	amtPrintErr = 0;
-	err_init_print = OS_NO_ERR;
-
-	if (err_init_print == OS_NO_ERR) {
-		BSP_USART_Init (APP_USART_PRINT_COM, usartSpeed);  // start with something slow, later increase
-
-		if (!(SerialPrintQSem = OSSemCreate(0)))
-		// after initialisation, probable a character can be sent
-		// worst case we just loose it
-		{
-			//err_printf("could not create serialQSem\n");
-			err_init_print = 0xFF;
-		}
-	}
-	
-	if (err_init_print == OS_NO_ERR)  {
-		#ifdef use_USART_PRINT_PDCA
-		initPDCA(&err_init_print);
-		#endif
-	}
-	memset(serialPrintStrBuf,0x00,sizeof(serialPrintStrBuf));
-	
-	if (err_init_print == OS_NO_ERR) {
-		serialPrintMsgMem = OSMemCreate(&serialPrintStrBuf[0], serialPrintStrBufSz, sizeof(serialPrintMem), &err_init_print);
-	}
-	if (err_init_print == OS_NO_ERR) {
-		OSMemNameSet(serialPrintMsgMem, (INT8U*)"serialMsgMem", &err_init_print);
-	}
-	if (err_init_print == OS_NO_ERR) {
-		serialPrintMsgTaskQ = OSQCreate(&serialPrintTaskQMsg[0], serialPrintStrBufSz);
-		if (! serialPrintMsgTaskQ) err_init_print = 0xFF;
-	}
-
-	#ifdef use_USART_TxRdyInt
-
-	if (err_init_print == OS_NO_ERR) {
-		if (BSP_INTC_IntReg(&APP_USARTRxTxISR, App_IRQ_USART, 1) == BSP_INTC_ERR_NONE ) {
-			err_init_print = OS_NO_ERR;
-			} else {
-			err_init_print = 0xFF;
-		}
-		BSP_USART_IntEn (APP_USART_PRINT_COM, (1<< AVR32_USART_IER_TXRDY));
-	}
-	#endif
-	
-	if (err_init_print == OS_NO_ERR )  {
-		err_init_print = OSTaskCreateExt(SerialQMethod,
-		(void *)0,
-		(OS_STK *)&SerialPrintQMethodStk[SerialpRrintQMethod_STK_SIZE - 1],
-		SerialQ_TASK_PRIO,
-		SerialQ_TASK_PRIO,
-		(OS_STK *)&SerialPrintQMethodStk[0],
-		SerialpRrintQMethod_STK_SIZE,
-		(void *)0,
-		OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR);
-	}
-	
-	OSTaskNameSet(SerialQ_TASK_PRIO, (INT8U *)"SerQ", &err_init_print);
-	serialPrintOn = (err_init_print == OS_NO_ERR);
-	
-	*/
 	return err_init_hygrosens;
 }
